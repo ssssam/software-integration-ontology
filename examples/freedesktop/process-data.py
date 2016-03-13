69127129614d0cb4138eb9076b5631a8ec953f96
Sam Thursfield <sam@afuera.me.uk>
2016-03-13 09:50:32 +0000

    Update "freedesktop stack" data
    Added WebKit, added some more properties, and prettified the generated JSON.

--- examples/freedesktop/process-data.py
+++ examples/freedesktop/process-data.py
@@ -36,17 +36,36 @@ def process_project(project, call_ohloh=None):
 
         total_commit_count = int(ohloh_data.getElementsByTagName(
             'total_commit_count')[0].firstChild.data)
+        total_code_lines = int(ohloh_data.getElementsByTagName(
+            'total_code_lines')[0].firstChild.data)
+        total_contributor_count = int(ohloh_data.getElementsByTagName(
+            'total_contributor_count')[0].firstChild.data)
 
         project['total_commit_count'] = total_commit_count
+        project['total_code_lines'] = total_code_lines
+        project['total_contributor_count'] = total_contributor_count
 
     if 'children' in project:
         for child in project['children']:
             process_project(child, call_ohloh)
 
-        if 'total_commit_count' not in project:
-            child_commit_count = sum(
-                child['total_commit_count'] for child in project['children'])
-            project['total_commit_count'] = child_commit_count
+    def propagate_property(name, project):
+        '''Calculate a property from children's values, if needed.
+
+        This doesn't do any 'deduplication', of course, so if a project
+        is made up of 4 component projects all of which are developed by
+        the same folk, those folk will be counted 4 times.
+
+        '''
+        if name not in project:
+            if 'children' in project:
+                value = sum(
+                    child[name] for child in project['children'])
+                project[name] = value
+
+    propagate_property('total_commit_count', project)
+    propagate_property('total_code_lines', project)
+    propagate_property('total_contributor_count', project)
 
 
 def main():
@@ -74,7 +93,7 @@ def main():
     for project in data['@graph']:
         process_project(project, call_ohloh=call_ohloh)
 
-    json.dump(data, sys.stdout)
+    json.dump(data, sys.stdout, indent=4)
 
 
 main()
